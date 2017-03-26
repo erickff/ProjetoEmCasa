c69b7a2e92d3e2e05d86b49a8e0eb632b784fde3
erick <erick@DESKTOP-K41643R>
2017-03-26 18:28:18 -0300

    Add project files.

---
+++ ProjetoemCasa/FormLogin.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ProjetoemCasa
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lbSenha = new System.Windows.Forms.Label();
+            this.mskSenha = new System.Windows.Forms.MaskedTextBox();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(353, 114);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(132, 42);
+            this.btnEntrar.TabIndex = 7;
+            this.btnEntrar.Text = "Login";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(165, 114);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(132, 42);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lbSenha
+            // 
+            this.lbSenha.AutoSize = true;
+            this.lbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenha.Location = new System.Drawing.Point(11, 71);
+            this.lbSenha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbSenha.Name = "lbSenha";
+            this.lbSenha.Size = new System.Drawing.Size(121, 18);
+            this.lbSenha.TabIndex = 9;
+            this.lbSenha.Text = "Senha de rede:";
+            // 
+            // mskSenha
+            // 
+            this.mskSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mskSenha.Location = new System.Drawing.Point(165, 71);
+            this.mskSenha.Margin = new System.Windows.Forms.Padding(4);
+            this.mskSenha.Name = "mskSenha";
+            this.mskSenha.PasswordChar = '*';
+            this.mskSenha.Size = new System.Drawing.Size(319, 29);
+            this.mskSenha.TabIndex = 6;
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsuario.Location = new System.Drawing.Point(165, 28);
+            this.tbUsuario.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(319, 29);
+            this.tbUsuario.TabIndex = 4;
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsuario.Location = new System.Drawing.Point(11, 28);
+            this.lbUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(72, 18);
+            this.lbUsuario.TabIndex = 5;
+            this.lbUsuario.Text = "Usuário:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 180);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.lbSenha);
+            this.Controls.Add(this.mskSenha);
+            this.Controls.Add(this.tbUsuario);
+            this.Controls.Add(this.lbUsuario);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lbSenha;
+        private System.Windows.Forms.MaskedTextBox mskSenha;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.Label lbUsuario;
+    }
+}
+

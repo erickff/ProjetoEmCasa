28b1bda79006878567b604876359cb8709e05a0a
erick <erick@DESKTOP-K41643R>
2017-03-26 18:56:51 -0300

    Novo form para escolha de produto

--- ProjetoemCasa/FormEscolheTeste.Designer.cs
+++ ProjetoemCasa/FormEscolheTeste.Designer.cs
@@ -28,24 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbProduto = new System.Windows.Forms.Label();
+            this.lbTeste = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btIniciar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lbProduto
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lbProduto.AutoSize = true;
+            this.lbProduto.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbProduto.Location = new System.Drawing.Point(12, 42);
+            this.lbProduto.Name = "lbProduto";
+            this.lbProduto.Size = new System.Drawing.Size(104, 27);
+            this.lbProduto.TabIndex = 0;
+            this.lbProduto.Text = "Produto:";
+            // 
+            // lbTeste
+            // 
+            this.lbTeste.AutoSize = true;
+            this.lbTeste.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTeste.Location = new System.Drawing.Point(41, 102);
+            this.lbTeste.Name = "lbTeste";
+            this.lbTeste.Size = new System.Drawing.Size(75, 27);
+            this.lbTeste.TabIndex = 1;
+            this.lbTeste.Text = "Teste:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(139, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(406, 34);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(139, 95);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(406, 34);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.Location = new System.Drawing.Point(139, 146);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(406, 42);
+            this.btIniciar.TabIndex = 4;
+            this.btIniciar.Text = "Iniciar";
+            this.btIniciar.UseVisualStyleBackColor = true;
             // 
             // FormEscolheTeste
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(701, 371);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(658, 251);
+            this.Controls.Add(this.btIniciar);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbTeste);
+            this.Controls.Add(this.lbProduto);
             this.Name = "FormEscolheTeste";
             this.Text = "FormEscolheTeste";
             this.ResumeLayout(false);
@@ -55,6 +101,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbProduto;
+        private System.Windows.Forms.Label lbTeste;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btIniciar;
     }
 }
